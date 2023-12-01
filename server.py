d6cc65d09ca619df0f714b42307e2100bb5e3bdd
Karthik Sreedhar <karthiksreedhar@berkeley.edu>
2023-11-30 22:47:06 -0500

    new

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ import os
 import openai
 
 import secrets
-openai.api_key = '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'
+openai.api_key = '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'
 
 
 
@@ -26,9 +26,15 @@ headline_data = {
   "content":"",
   "tone":"",
   "subject":"",
+  "last":"",
+  "new":"",
+  "lastlast":"",
   "generations":"",
-  "cache":[]
+  "cache":[],
+  "feedback":""
 }
+
+# headline_data["generations"] = "Columbia master’s student interested in collaborating on research \nDear Dr. Chilton,\nMy name is Karthik Sreedhar and I am a computer science and journalism master’s student at Columbia University. I am reaching out because I am interested in discussing the possibility of collaborating on research exploring human-computer interaction and generative AI. I am fascinated by the ways in which the research projects at the Computational Design Lab utilize technology to make individuals’ everyday lives easier. \nMy prior research has overlap with your work; in the past, I worked with Professor Armando Fox on a human-computer interaction study that aimed to improve computer science instruction and evaluation of adherence to agile methodology by student teams. I am eager to see how I could apply the same techniques to contribute to building user-focused technologies that use generative AI, especially in the context of journalism, as I noticed you had worked on several relevant projects.\nI would love to have a brief conversation in the next week to learn more about your research and discuss how my project experience and research knowledge could be beneficial in advancing your academic work.\nSincerely,\nKarthik Sreedhar"
 
 # This is what the representation looks like when there are keywords and images generated
 # sample_headline_data_2 = {
@@ -101,6 +107,21 @@ def increase_formality():
     prompt = "Rewrite the following email with more formal language: \n" + headline_data["generations"]
     response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=256)["choices"][0]["text"]
     headline_data["generations"] = response
+
+    return jsonify(headline_data)
+
+@app.route('/give_feedback', methods=['GET', 'POST'])
+def give_feedback():
+
+    recipient = headline_data["headline"]
+    relation = headline_data["new"]
+    message = headline_data["last"]
+    reason = headline_data["lastlast"]
+    email = headline_data["generations"]
+
+    prompt = f"Provide constructive criticism in list format about this email, written to {recipient} from one of her students: {email}"
+    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=256)["choices"][0]["text"]
+    headline_data["feedback"] = response
 
     return jsonify(headline_data)
 
@@ -289,6 +310,79 @@ def get_keywords():
     #send back the WHOLE array of data, so the client can redisplay it
     return jsonify(headline_data)
 
+@app.route('/first_submission', methods=['GET', 'POST'])
+def first_submission():
+    global headline_data
+    data = request.get_json()   
+
+    headline_data["headline"] = data["headline"]
+
+    #send back the WHOLE array of data, so the client can redisplay it
+    return jsonify(headline_data)
+
+@app.route('/second_submission', methods=['GET', 'POST'])
+def second_submission():
+    global headline_data
+    data = request.get_json()   
+
+    print('in the sub')
+
+    headline_data["new"] = data["new"]
+    headline_data["last"] = data["last"]
+    headline_data["lastlast"] = data["lastlast"]
+
+    headline_data["generations"] = generate_email_new_(headline_data)
+    if headline_data["generations"] not in headline_data["cache"]: headline_data["cache"].append(headline_data["generations"])
+
+    # headline_data["generations"] = "Subject: Columbia master’s student interested in collaborating on research \nDear Dr. Chilton,\nMy name is Karthik Sreedhar and I am a computer science and journalism master’s student at Columbia University. I am reaching out because I am interested in discussing the possibility of collaborating on research exploring human-computer interaction and generative AI. I am fascinated by the ways in which the research projects at the Computational Design Lab utilize technology to make individuals’ everyday lives easier. \nMy prior research has overlap with your work; in the past, I worked with Professor Armando Fox on a human-computer interaction study that aimed to improve computer science instruction and evaluation of adherence to agile methodology by student teams. I am eager to see how I could apply the same techniques to contribute to building user-focused technologies that use generative AI, especially in the context of journalism, as I noticed you had worked on several relevant projects.\nI would love to have a brief conversation in the next week to learn more about your research and discuss how my project experience and research knowledge could be beneficial in advancing your academic work.\nSincerely,\nKarthik Sreedhar"
+
+    #send back the WHOLE array of data, so the client can redisplay it
+    return jsonify(headline_data)
+
+def generate_email_new_(headline_data):
+    recipient = headline_data["headline"]
+    relation = headline_data["new"]
+    message = headline_data["last"]
+    reason = headline_data["lastlast"]
+
+    prompt = f"Write a short email, with a subject line, for me addressed to {recipient}, whom I know because {relation}. I am writing because {message}. The reason for this request is {reason}."
+    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=256)["choices"][0]["text"]
+
+    return response
+
+
+def generate_email_new_new(headline_data):
+    recipient = headline_data["headline"]
+    intro = headline_data["new"]
+    motivation = headline_data["content"]
+    priorExp = headline_data["tone"]
+    profExp = headline_data["subject"]
+    ask = headline_data["last"]
+
+    # website = f"Summarize this website: {profExp}"
+    # website = f"What does {recipient} work on? Only include information you are confident about - otherwise you will be fired."
+    # summary = openai.Completion.create(engine="text-davinci-003", prompt=website, max_tokens=256)["choices"][0]["text"]
+
+    summary = "Lydia Chilton’s area of study is human-computer interaction with a focus on computational design, including viewing the design process from a computational standpoint. Two current projects are constructing visual metaphors for creative ads and using computational tools to write humor and news satire."
+
+    # print(summary)
+
+    mywork = f"Can you summarize this role I have had in the past for me? Here is the role with the title and company in the first line, and the description in bullet points: {priorExp}"
+    myworksum = openai.Completion.create(engine="text-davinci-003", prompt=mywork, max_tokens=256)["choices"][0]["text"]
+
+    print(myworksum)
+
+    connection_ = f"Make a connection between {recipient} and myself. Here is information about {recipient}: {summary}. Here is information about myself: {myworksum}"
+    connection__ = openai.Completion.create(engine="text-davinci-003", prompt=connection_, max_tokens=256)["choices"][0]["text"]
+
+    print(connection__)
+
+    prompt = f"Help me write a cold-email to {recipient}. I am reaching out because {motivation}. The connection between my own work and {recipient} is here: {connection__}. Please reference at least one specific line of research of {recipient} in the email. My specific ask is {ask}. Use the next sentence as an introduction: {intro}."
+    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=256)["choices"][0]["text"]
+
+    print(response)
+
+    return response
 
 def parse_keywords_from_gpt_response(keyword_response):    
     keyword_list = keyword_response.splitlines()
